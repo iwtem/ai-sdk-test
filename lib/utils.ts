269936b1f045a91a00a9c95e9c48b78441e1acc2
huibin <iwtem@aliyun.com>
2026-03-16 10:15:40 +0800

    feat: add customAlphabet function for generating unique IDs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { customAlphabet } from "nanoid";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
